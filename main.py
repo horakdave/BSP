a67b646c20a4d7b8d5143de48ad4b93f9ac189f3
horakdave <horakdejv@gmail.com>
2024-08-19 21:55:10 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from wad_data import WADData
+from settings import *
+import pygame as pg
+import sys
+from map_renderer import MapRenderer
+from player import Player
+from bsp import BSP
+
+
+class DoomEngine:
+    def __init__(self, wad_path='wad/DOOM1.WAD'):
+        self.wad_path = wad_path
+        self.screen = pg.display.set_mode(WIN_RES)
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.dt = 1 / 60
+        self.on_init()
+
+    def on_init(self):
+        self.wad_data = WADData(self, map_name='E1M1')
+        self.map_renderer = MapRenderer(self)
+        self.player = Player(self)
+        self.bsp = BSP(self)
+
+    def update(self):
+        self.player.update()
+        self.bsp.update()
+        self.dt = self.clock.tick()
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+        self.map_renderer.draw()
+        pg.display.flip()
+
+    def check_events(self):
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                self.running = False
+
+    def run(self):
+        while self.running:
+            self.check_events()
+            self.update()
+            self.draw()
+        pg.quit()
+        sys.exit()
+
+
+if __name__ == '__main__':
+    doom = DoomEngine()
+    doom.run()
